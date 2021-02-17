988df1664c5d254cecb646c117fc2a0603bc1aa2
kennyOlakunle <olakunlemuyiwa@gmail.com>
2021-02-17 14:09:43 +0100

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import todos from './modules/todos';
+
+//load vuex
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: {
+    todos,
+  },
+});
