086f3f4b003a2dce144584a7121dd1e39fa53484
kennyOlakunle <olakunlemuyiwa@gmail.com>
2021-02-17 14:46:31 +0100

    making UI responsive

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,6 +2,16 @@
 <div>
 
     <h3>Todos</h3>
+<div class="legend">
+    <span>Double click to mark as complete</span>
+    <span>
+        <span class="incomplete-box"></span> = Incomplete
+    </span>
+    <span>
+        <span class="complete-box"></span> = Complete
+    </span>
+</div>
+
     <div class="todos">
         <div v-for="todo in allTodos" :key="todo.id" class="todo">
             {{todo.title}} 
@@ -51,6 +61,36 @@ i {
    color: #fff;
    cursor: pointer; 
 }
+
+.legend {
+display: flex;
+justify-content: space-around;
+margin-bottom: 1rem;
+}
+
+.complete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: #35495e;
+
+}
+
+.incomplete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: #41b883;
+}
+
+@media (max-width: 500px){
+    .todos {
+        display: grid;
+        grid-template-columns: 1fr;
+    }
+    
+}
+
 
 </style>
 
