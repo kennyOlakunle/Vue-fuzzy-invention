1b8ec28cdac281af951b2be6fc021e5f61bc57ca
kennyOlakunle <olakunlemuyiwa@gmail.com>
2021-02-17 14:34:42 +0100

    adding filter and delete features

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -30,6 +30,18 @@ const actions = {
 
     commit('removeTodo', id);
   },
+
+  async filterTodos({ commit }, e) {
+    // Get selected No
+
+    const limit = parseInt(
+      e.target.options[e.target.options.selectedIndex].innerText
+    );
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
+    );
+    commit('setTodos', response.data);
+  },
 };
 
 const mutations = {
